a623c447eedfdbccf2e33d2f8af8ca303abfd7f0
Nickolas Farias <nickolasfarias@gmail.com>
2022-06-23 19:35:19 -0300

    seed

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,5 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
-  
   validates :name, uniqueness: true, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+pages = (1..500).to_a
+
+pages.each_with_index do |page, i|
+  p "Getting movies from page #{i + 1}"
+  url = "https://api.themoviedb.org/3/movie/top_rated?api_key=854a35589ffa7bd74416b849a8ab8f11&language=en-US&page=#{page}"
+  movies_serialized = URI.open(url).read
+  movies = JSON.parse(movies_serialized)
+  movies["results"].each do |result|
+    Movie.create(title: result["title"], overview: result["overview"], poster_url: "https://image.tmdb.org/t/p/w300#{result['poster_path']}", rating: result["vote_average"])
+  end
+  p "Finished"
+end
