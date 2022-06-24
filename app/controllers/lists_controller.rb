b3096db69e83a4b2522637f56d62675a121b23f2
Nickolas Farias <nickolasfarias@gmail.com>
2022-06-23 22:40:31 -0300

    cloudinary

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,17 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
