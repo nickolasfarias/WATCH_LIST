a6f365fa41044a7717af170126bdef9bf90401e6
Nickolas Farias <nickolasfarias@gmail.com>
2022-06-24 21:18:16 -0300

    show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "my_lists", to: "lists#my"
+  get "o_list/:id", to: "lists#othershow", as: :other_show
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
